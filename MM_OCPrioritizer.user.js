e43827abb07db80e596cbbc2bccb4ddf19045bd8
Norm2390 <funfdreizwei@gmail.com>
2026-08-17 01:19:31 -0400

    Update MM_OCPrioritizer.user.js

--- MM_OCPrioritizer.user.js
+++ MM_OCPrioritizer.user.js
@@ -5,10 +5,7 @@
 // @description  Faction CPR requirements + role qualification highlighting + role weights + OC card reordering for Torn OC 2.0. All local, no API, information off your crime page.
 // @match        https://www.torn.com/factions.php*
 // @run-at       document-end
-// @grant        GM.xmlHttpRequest
-// @grant        GM_setValue
-// @grant        GM_getValue
-// @grant        GM_info
+// @grant        one
 // @downloadURL  https://github.com/Norm2390/MonarchScripts/raw/refs/heads/main/MM_OCPrioritizer.user.js
 // @updateURL    https://github.com/Norm2390/MonarchScripts/raw/refs/heads/main/MM_OCPrioritizer.user.js
 // @author       Jocko [55408]
